43120f4f16871228e5ed8435c439b4e4908e018f
d34d <84479990+Vajean1@users.noreply.github.com>
2022-09-13 19:00:10 -0300

    Atualizações e aplicações de algumas mudanças

--- botton.py
+++ botton.py
@@ -1,21 +1,30 @@
+from genericpath import isfile
 from pytube.cli import on_progress
 from pytube import YouTube
 from moviepy.editor import *
+from time import sleep
+import threading
 import os
 
 #Verifica e cria pasta para alocar o bot.
 def criarPasta():
-    if os.path.exists('bot'):
+    if os.path.exists('BOT_TIKTOK'):
         print('Pasta já criada.')
     else:
         print('Criando pasta...')
-        os.mkdir('bot')
+        os.mkdir('BOT_TIKTOK')
         #Movendo para pasta indicada
         local_atual = f'{os.getcwd()}\\botton.py'
-        local_destino = f'{os.getcwd()}\\bot\\botton.py'
+        local_destino = f'{os.getcwd()}\\BOT_TIKTOK\\botton.py'
         os.rename(local_atual, local_destino)
+    
+    # verifica se os vídeos ainda estão lá dentro, caso estejam, excluir eles
+    if os.path.isfile('./BOT_TIKTOK/video.mp4'):
+        os.remove('./BOT_TIKTOK/video.mp4')
+    if os.path.isfile('./BOT_TIKTOK/video_pronto.mp4'):
+        os.remove('./BOT_TIKTOK/video_pronto.mp4')
 
-criarPasta()
+threading.Thread(target=criarPasta).start()
 
 url = input(str('Cole aqui a URL do vídeo: '))
 
@@ -28,14 +37,14 @@ def baixar_video(url=str):
         print('Baixando...')
         video.download()
         print('Download Finalizado!')
-    except:
-        print('Error')
+    except Exception as err:
+        print(f'Error: {err}')
     #Renomeando
     vd = f'{yt.title}.mp4'
     os.rename(vd, 'video.mp4')
     #Movendo para pasta indicada
     local_atual = f'{os.getcwd()}\\video.mp4'
-    local_destino = f'{os.getcwd()}\\bot\\video.mp4'
+    local_destino = f'{os.getcwd()}\\BOT_TIKTOK\\video.mp4'
     os.rename(local_atual, local_destino)
 
 baixar_video(url)
@@ -45,14 +54,11 @@ def cortarVideo(url):
         clip = VideoFileClip(url)
         clip = clip.subclip(50, 80)
         clip.write_videofile('video_pronto.mp4')
-    except:
-        print('Não foi possível editar: ')
+    except Exception as err:
+        print(f'Não foi possível editar: {err}')
 
     local_atual = f'{os.getcwd()}\\video_pronto.mp4'
-    local_destino = f'{os.getcwd()}\\bot\\video_pronto.mp4'
+    local_destino = f'{os.getcwd()}\\BOT_TIKTOK\\video_pronto.mp4'
     os.rename(local_atual, local_destino)
 
-cortarVideo('./bot/video.mp4')
-
-if os.path.exists('./bot/video.mp4'):
-    os.remove('./bot/video.mp4')
+cortarVideo('./BOT_TIKTOK/video.mp4')
